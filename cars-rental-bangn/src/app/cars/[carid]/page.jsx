3db97fd81743f1a364208c53e36ee3f3105b7b6a
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2025-12-20 12:00:53 +0700

    add id render for specific carid in the carInfo component

--- cars-rental-bangn/src/app/cars/[carid]/page.jsx
+++ cars-rental-bangn/src/app/cars/[carid]/page.jsx
@@ -1,5 +1,11 @@
-function CarInfo() {
-  return <div>CarId</div>;
+async function CarInfo({ params }) {
+  const { carid } = await params;
+  return (
+    <div>
+      <h1>Car Information</h1>
+      <h2>Car Id: {carid}</h2>
+    </div>
+  );
 }
 
 export default CarInfo;
