1d894fc06141817c084af54d4153d5d3a7c229e2
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2025-12-24 20:51:36 +0700

    add register logic for BE as register endpoint

---
+++ cars-rental-bangn/src/app/api/users/register/route.js
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connectDB } from "@/config/dbConfig";
+import bcrypt from "bcryptjs";
+
+connectDB();
+
+export async function POST(request) {
+  try {
+    // access request body
+    const reqBody = await request.json();
+    const { name, email, password } = reqBody;
+
+    // check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists", status: 400 },
+        { status: 400 }
+      );
+    }
+
+    // create new user
+    // hash password, create new user and save to database
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    return NextResponse.json(
+      { message: "User registered successfully", status: 201 },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error", status: 500 },
+      { status: 500 }
+    );
+  }
+}
