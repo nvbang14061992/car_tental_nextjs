1ee789b8ee261a643ac9e4b8c8ab077252db881c
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2025-12-25 22:15:10 +0700

    add logic to remove navbar from login and register page

--- cars-rental-bangn/src/app/page.js
+++ cars-rental-bangn/src/app/page.js
@@ -1,52 +1,9 @@
-import axios from "axios";
 import CustomButton from "../components/CustomButton";
-import { cookies } from "next/dist/client/components/headers";
-
-export async function getCurrentUser() {
-  try {
-    let baseUrl;
-    const cookieStore = cookies();
-    const token = cookieStore.get("token");
-
-    // check if dev or prod environment
-    if (process.env.NODE_ENV === "development") {
-      console.log("Development environment");
-      baseUrl = "http://localhost:3000";
-    } else {
-      console.log("Production environment");
-      baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-    }
-
-    // this is server side rendering, so there is no browser to add cookies and domain for url automatically
-    // thus we have to manually add cookie header with token
-    const response = await axios.get(`${baseUrl}/api/users/currentuser`, {
-      headers: {
-        Cookie: `token=${token?.value}`,
-      },
-    });
-
-    const data = response.data;
-    return data.data;
-  } catch (error) {
-    console.error("Error fetching current user:", error);
-    return null;
-  }
-}
 
 export default async function Home() {
-  const currentUser = await getCurrentUser();
-  console.log("Home");
   return (
     <div>
       <h1>Home Page</h1>
-      {currentUser ? (
-        <div>
-          <h2>Welcome, {currentUser.name}!</h2>
-          <p>Email: {currentUser.email}</p>
-        </div>
-      ) : (
-        <p></p>
-      )}
     </div>
   );
 }
--- cars-rental-bangn/src/components/LayoutProvider.jsx
+++ cars-rental-bangn/src/components/LayoutProvider.jsx
@@ -1,11 +1,29 @@
 "use client";
 
-import { ConfigProvider } from "antd";
-import React from "react";
-import { useRouter } from "next/navigation";
+import { ConfigProvider, message } from "antd";
+import React, { useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import axios from "axios";
 
 function LayoutProvider({ children }) {
+  const [user, setUser] = useState(null);
   const router = useRouter();
+  const pathname = usePathname();
+  const getCurrentUser = async () => {
+    try {
+      const response = await axios.get("/api/users/currentuser");
+      setUser(response.data.data);
+    } catch (error) {
+      message.error(error.response?.data?.message || error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (pathname !== "/login" && pathname !== "/register") {
+      getCurrentUser();
+    }
+  }, []);
+
   return (
     <div>
       <ConfigProvider
@@ -15,19 +33,21 @@ function LayoutProvider({ children }) {
           },
         }}
       >
-        <div className="header bg-primary p-3 flex justify-between">
-          <h1 className="text-xl text-white">BangCars</h1>
-          <div className="flex gap-5 items-center">
-            <h1
-              className="text-md text-white underline"
-              onClick={() => router.push("/profile")}
-            >
-              Username
-            </h1>
-            <i className="ri-logout-box-r-line text-white"></i>
+        {pathname !== "/login" && pathname !== "/register" && (
+          <div className="header bg-primary p-3 flex justify-between items-center">
+            <h1 className="text-xl text-white">BangCars</h1>
+            <div className="flex gap-5 items-center">
+              <h1
+                className="text-md text-white underline"
+                onClick={() => router.push("/profile")}
+              >
+                {user?.name}
+              </h1>
+              <i className="ri-logout-box-r-line text-white"></i>
+            </div>
           </div>
-        </div>
-        {children}
+        )}
+        <div>{children}</div>
       </ConfigProvider>
     </div>
   );
